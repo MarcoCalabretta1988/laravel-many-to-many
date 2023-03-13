48f1078fcd79a759197f6c16ee25cdaa4f81df6e
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-03-13 13:21:09 +0100

    first commit

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $type_ids = Type::select('id')->pluck('id')->toArray();
+        $type_ids[] = null;
+
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->type_id = Arr::random($type_ids);
+            $project->name = $faker->words(3, true);
+            $project->description = $faker->paragraphs(6, true);
+            $project->github = "https://github.com/MarcoCalabretta1988";
+            $project->linkedin = "www.linkedin.com/in/marco-calabretta-2b1b13195";
+            $project->save();
+        }
+    }
+}
---
+++ resources/views/admin/types/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title' , 'Edit type')
+
+@section('content')
+
+<h1 class="text-white text-center py-5">- Edit type -</h1>
+<div class="bg-dark text-white p-5">
+    @include('includes.types.form')
+</div>
+
+
+
+@endsection
+
